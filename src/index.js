556269f1c9e430e009b635ae821895214a8d1bb7
Bassam Hashmi <bassamhashmi7@gmail.com>
2022-08-10 13:47:52 +0500

    optimized

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ActiveGenreProvider } from "./context/activeGenreContext";
-import { MoviesDataProvider } from "./context/moviesDataContext";
+import ContextProvider from "./context/contextProvider";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ActiveGenreProvider>
-      <MoviesDataProvider>
-        <App />
-      </MoviesDataProvider>
-    </ActiveGenreProvider>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>
 );
